576829040c52d580ba0c683285542fba2276210f
ransomware3 <natanbaldo11@gmail.com>
2022-10-17 09:39:21 -0300

    protótipo inincial

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const btnVoltar = document.getElementById('seta-voltar')
+const btnAvancar = document.getElementById('seta-avancar')
+const secoes = document.querySelectorAll('.image')
+let secaoAtual = 0
+
+function esconderSecaoSelecionada(){
+    const sessaoSelecionada = document.querySelector('.selected')
+    sessaoSelecionada.classList.remove('selected')
+}
+
+function mostrarSecao(indiceSecao){
+    secoes[indiceSecao].classList.add('selected')
+}
+
+btnVoltar.addEventListener('click', function(){
+    if(secaoAtual === 0) return
+
+    esconderSecaoSelecionada()
+
+    secaoAtual--
+    mostrarSecao(secaoAtual)
+})
+
+btnAvancar.addEventListener('click', function(){
+    if(secaoAtual === secoes.length -1) return
+
+    esconderSecaoSelecionada()
+
+    secaoAtual++
+    mostrarSecao(secaoAtual)
+})
